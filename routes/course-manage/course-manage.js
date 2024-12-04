e29546e78ab3f4827b9857cf9efb6403beb589cd
Than Viet Anh <vietanh.vadc.2004@gmail.com>
2024-12-04 14:08:29 +0700

    cmd

---
+++ routes/course-manage/course-manage.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import courseManageController from '../../controllers/courseManage/courseManage.controller.js';
+
+const router = express.Router();
+
+router.get('/courses', courseManageController.getAllCourses);
+router.post('/courses/add', courseManageController.createCourse);
+router.get('/teachers', courseManageController.getAllTeachers);
+router.get('/students', courseManageController.getAllStudents);
+router.get('/terms', courseManageController.getAllTerms);
+router.get('/courses/:course_id', courseManageController.getCourseById);
+router.put('/courses/:course_id', courseManageController.updateCourse);
+router.delete('/courses/:course_id', courseManageController.deleteCourse);
+
+router.get('/users-courses/:user_id', courseManageController.getLoginedUsersCourses);
+
+router.post('/courses-in-term-teacher', courseManageController.getCoursesInTermOfTeacher);
+
+router.post('/courses-in-term-student', courseManageController.getCoursesInTermOfStudent);
+
+export default router;
